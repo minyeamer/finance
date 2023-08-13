11fc07e3d81946afd909729b3ec5d74b5d7a15b3
minyeamer <minyeamer@gmail.com>
2023-08-14 01:22:35 +0900

    feat: add yahoo price spider

--- parsers/yahoo.py
+++ parsers/yahoo.py
@@ -1,12 +1,17 @@
-from gscraper.base import Parser
-from gscraper.date import now
-from gscraper.map import hier_get, filter_map
+from gscraper.base import Parser, log_results
+from gscraper.date import now, get_date
+from gscraper.map import hier_get, filter_map, filter_data
 
 from typing import Dict
 import json
+import pandas as pd
 
 YAHOO = "yahoo"
-EST = "EST"
+EST = "US/Eastern"
+
+PRICE_RENAME = {"Date":"date", "Datetime":"datetime",
+                "Open":"open", "High":"high", "Low":"low", "Close":"close", "Volume":"volume"}
+PRICE_COLUMNS = ["open", "high", "low", "close"]
 
 
 class YahooQueryParser(Parser):
@@ -32,3 +37,19 @@ class YahooSummaryParser(Parser):
                     "description": data["longBusinessSummary"],
                     "updateDate": now(tzinfo=EST).date(), "updateTime": now(tzinfo=EST)}
         return filter_map(profile, filter=filter)
+
+
+class YahooPriceParser(Parser):
+    operation = "yahooPrice"
+
+    def parse(self, response: pd.DataFrame, symbol=str(), trunc=2, filter=list(), **kwargs) -> pd.DataFrame:
+        if response.empty:
+            log_results(list(), symbol=symbol)
+            return pd.DataFrame()
+        data = response.reset_index().rename(columns=PRICE_RENAME)
+        data["symbol"] = symbol
+        if isinstance(trunc, int):
+            for column in PRICE_COLUMNS:
+                if column in data: data[column] = data[column].apply(lambda x: round(x, trunc))
+        log_results(data, symbol=symbol)
+        return filter_data(data, filter=filter, return_type="dataframe")
--- spiders/yahoo.py
+++ spiders/yahoo.py
@@ -1,19 +1,25 @@
-from gscraper.base import get_headers, log_messages, log_client
+from gscraper.base import AsyncSpider, get_headers, log_messages, log_client
+from gscraper.cast import cast_tuple
+from gscraper.date import now, get_date
+from gscraper.map import unique
 
 from base.spiders import FinanceAsyncSpider
 from base.urls import API_URL, GET_URL
+from data.models import US_STOCK_PRICE_SCHEMA
 from data.yahoo import *
 from parsers.yahoo import *
 
-from typing import Dict
+from typing import Dict, List, Tuple
 from aiohttp import ClientSession
+import datetime as dt
+import yfinance
 
 YAHOO = "yahoo"
 
 
 class YahooQuerySpider(FinanceAsyncSpider, YahooQueryParser):
     operation = "yahooQuery"
-    message = "Collecting stock information"
+    message = "Collecting stock information from yahoo finance"
 
     @FinanceAsyncSpider.asyncio_errors
     @FinanceAsyncSpider.asyncio_limit
@@ -29,7 +35,7 @@ class YahooQuerySpider(FinanceAsyncSpider, YahooQueryParser):
 
 class YahooSummarySpider(FinanceAsyncSpider, YahooSummaryParser):
     operation = "yahooSummary"
-    message = "Collecting company profiles"
+    message = "Collecting company profiles from yahoo finance"
 
     @FinanceAsyncSpider.asyncio_errors
     @FinanceAsyncSpider.asyncio_limit
@@ -41,3 +47,53 @@ class YahooSummarySpider(FinanceAsyncSpider, YahooSummaryParser):
         async with session.get(api_url, headers=headers) as response:
             self.logger.info(await log_client(response, url=api_url, symbol=symbol))
             return self.parse(await response.text(), symbol, **kwargs)
+
+
+DATE_LIMIT = {"1m":30, "2m":60, "5m":60, "15m":60, "30m":60, "60m":730, "90m":60,
+                "1h":730, "1d":None, "5d":None, "1wk":None, "1mo":None, "3mo":None}
+
+class YahooPriceSpider(AsyncSpider, YahooPriceParser):
+    operation = "yahooPrice"
+    returnType = "dataframe"
+    message = "Collecting yahoo stock prices"
+
+    @AsyncSpider.asyncio_session
+    async def crawl(self, query: List[str], start: dt.date=None, end: dt.date=None,
+                    freq="1d", **kwargs) -> pd.DataFrame:
+        start, end = self.set_date(start, end, freq)
+        return await self.gather(unique(*query), start, end, freq, **kwargs)
+
+    @AsyncSpider.asyncio_filter
+    async def gather(self, query: List[str], start: dt.date=None, end: dt.date=None,
+                    freq="1d", message=str(), **kwargs) -> pd.DataFrame:
+        message = message if message else self.message
+        return pd.concat(await self.tqdm.gather(
+            *[self.fetch(symbol, __start, __to, freq, **kwargs)
+                for symbol in query for __start, __to in self.set_period(start, end, freq)], desc=message))
+
+    @AsyncSpider.asyncio_errors
+    @AsyncSpider.asyncio_limit
+    async def fetch(self, symbol: str, start: dt.date=None, end: dt.date=None,
+                    freq="1d", prepost=False, trunc=2, **kwargs) -> pd.DataFrame:
+        response = yfinance.download(symbol, start, end, interval=freq, prepost=prepost, progress=False)
+        return self.parse(response, symbol, trunc, **kwargs)
+
+    def set_date(self, start=None, end=None, freq="1d", **kwargs) -> Tuple[dt.date,dt.date]:
+        start, end, today = get_date(start, default=None), get_date(end, default=None), now().date()
+        limit = DATE_LIMIT[freq]
+        if not limit: return start, end
+        is_end, end = bool(end), (end if end else today)
+        if not start: return (end-dt.timedelta(limit)), end
+        days = ((end if end else today)-start).days
+        if days <= limit: return start, end
+        elif is_end: return (end-dt.timedelta(limit)), end
+        else: return start, (start+dt.timedelta(limit))
+
+    def set_period(self, start=None, end=None, freq="1d", **kwargs) -> List[Tuple[dt.date,dt.date]]:
+        if freq != "1m": return [(start, end)]
+        starts = list(map(lambda x: x.date(), pd.date_range(start, end, freq="7D")))
+        return [(x, (y-dt.timedelta(days=1)))
+                for x, y in zip(starts, starts[1:]+[end+dt.timedelta(days=1)])]
+
+    def get_gbq_schema(self, freq="day", **kwargs) -> List[Dict[str, str]]:
+        return US_STOCK_PRICE_SCHEMA("time" if isinstance(freq, int) or "minute" in str(freq) else "date")
