afa4242f14487449680314b14ac086499f9a7d74
minyeamer <minyeamer@gmail.com>
2024-01-16 22:21:09 +0900

    update: add frequently used objects to init

---
+++ data/__init__.py
@@ -0,0 +1,7 @@
+from gscraper.base.abstract import Query, Variable
+from gscraper.base.session import Info, Schema, Field
+from gscraper.base.session import Apply, Match, Cast, Exists, Join, Regex, Rename, Split, Stat, Sum, Map
+from gscraper.base.spider import PipelineInfo, PipelineQuery, PipelineSchema, PipelineField
+from gscraper.base.gcloud import BigQuerySchema
+
+from gscraper.utils.map import flip_dict
---
+++ spiders/__init__.py
@@ -0,0 +1,7 @@
+from gscraper.base.session import Flow, Process, Match
+from gscraper.base.spider import Spider, AsyncSpider, EncryptedSpider, EncryptedAsyncSpider
+from gscraper.base.spider import INVALID_VALUE_MSG, get_headers
+
+from base.abstract import GET, POST, OPTIONS, HEAD, PUT, PATCH, DELETE, API, URL
+from base.abstract import ALPHA, NAVER, SQUARE, YAHOO
+from base.spider import FinanceSpider, FinanceKrSpider, FinanceAsyncSpider, FinanceKrAsyncSpider
